325372e2a5f81ed3a40ad2b8a6c337b3d5524c49
Raj Singh <rajvikrams@gmail.com>
2020-11-08 20:56:59 -0800

    makefile cleanup

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 
-clean:
-	\rm bin/*
 all:
 	go build -o bin/testMod main/main.go
+
+clean:
+	\rm bin/*
--- main/main.go
+++ main/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"../mymod"
+	"github.com/rajvikram/testMod/mymod"
 )
 
 func main() {
